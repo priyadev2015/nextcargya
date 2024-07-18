062b35ec784edb9f5ff7f87fff44c1ba00e41b74
priyadev2015 <162570878+priyadev2015@users.noreply.github.com>
2024-07-18 16:55:34 +0530

    Update users.tsx

--- my-app/lib/modal/users.tsx
+++ my-app/lib/modal/users.tsx
@@ -3,27 +3,27 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface UserDocument extends Document {
-  email: string;
-  password: string;
-  fullname: string;
-  phone: number;
-  identificationNumber: string;
+  name: string;
+  lastName: string;
+  documentNumber: string;
+  city: string;
   vehicleType?: string;
   vehicleLicensePlateNumber?: string;
-  fuelCode: string; // Add fuel code field
+  fuelCode: string;
   createdAt: Date;
   updatedAt: Date;
+ 
 }
 
 const userSchema = new Schema<UserDocument>({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  fullname: { type: String, required: true },
-  phone: { type: Number, required: true },
-  identificationNumber: { type: String, required: true },
+  name: { type: String, required: true },
+  lastName: { type: String, required: true },
+  documentNumber: { type: String, required: true },
+  city: { type: String, required: true },
   vehicleType: { type: String },
   vehicleLicensePlateNumber: { type: String },
-  fuelCode: { type: String, required: true }, // Define fuel code field
+  fuelCode: { type: String, required: true },
+ 
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
@@ -31,4 +31,3 @@ const userSchema = new Schema<UserDocument>({
 const User = mongoose.models.User || mongoose.model<UserDocument>('User', userSchema);
 
 export default User;
-
